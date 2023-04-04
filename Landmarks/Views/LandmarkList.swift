eb17a75b5dc80b8d11b6f758e46c59cdc8218820
JulioCoi <amorim.jc@gmail.com>
2023-04-04 20:28:22 -0300

    Implementa a recursividade em vários dispositivos Apple

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -24,6 +24,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 8","iPhone SE (2nd generation)", "iPhone 14 Pro", "iPad Air (5th generation)"], id: \.self) {
+            deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
