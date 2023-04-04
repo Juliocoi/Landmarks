7d07470cebf24b479e11f551e9f2ab69c531245c
JulioCoi <amorim.jc@gmail.com>
2023-04-04 00:27:54 -0300

    implementa a navegação entre lisas e detalhes, configura as informações de cada item da lista

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,38 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-                .ignoresSafeArea(edges: .top)
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-            
-                HStack {
-                    Text("Joshua Tree National Park.")
-                    
-                    Spacer()
-                    
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue interdum velit euismod in pellentesque massa placerat duis ultricies. Risus commodo viverra maecenas accumsan lacus vel.")
-                    
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
