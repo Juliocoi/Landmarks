e8d67172b028069b71f75533e9cb916e9a05a11b
JulioCoi <amorim.jc@gmail.com>
2023-04-04 00:26:35 -0300

    implementa a navegação entre lisas e detalhes, configura as informações de cada item da lista

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Júlio César Amorim on 03/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
